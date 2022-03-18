c2ffc15eb8329e494b051f7a30600e773bf71f2b
itimaru-itinokura <eakhoeajr@gmail.com>
2022-03-18 20:07:21 +0900

    first-commit

---
+++ js/script.js
@@ -0,0 +1,69 @@
+window.onunload = function () { };
+function sizecheck() {
+  w = window.innerWidth ? window.innerWidth : $(window).width();
+  h = window.innerHeight ? window.innerHeight : $(window).height();
+}
+// ハンバーガー
+sizecheck();
+$(function () {
+  if (w > 660) {
+} else { 
+  $("header .drawer").click(function(){
+  $('body').toggleClass('nav_open');
+    });
+  }
+}); 
+
+// アコーディオン
+$('.faq_box').click(function () {
+  if ($(this).hasClass('open')) {
+    let faq_answer = $(this).find('.faq_answer');
+    $('.faq_box').removeClass('open');
+    $('.faq_answer').slideUp();
+  }
+  else {
+    let faq_answer = $(this).find('.faq_answer');
+    $('.faq_box').removeClass('open');
+    $('.faq_answer').slideUp();
+    $(this).addClass('open');
+    faq_answer.slideDown();
+  }
+});
+
+
+// タイトルの下のライン
+function border() {
+  // 真ん中から広がるラインborderTrigger
+  $('.borderTrigger').each(function () {// borderTriggerクラスの各要素に対して
+    
+    let scroll = $(window).scrollTop();// スクロール位置を取得する
+    let triTop = $(this).offset().top + 100;// 要素の上部より100px下の位置を取得
+    let winHeight = $(window).height();// ウィンドウの高さを取得
+    if (scroll >= triTop - winHeight) { // 画面内に要素が入ったかを判断
+      $(this).addClass('border');// borderクラスを付与
+    } else {
+      $(this).removeClass('border');// borderクラスを削除
+    }
+  });
+}
+$(window).scroll(function () {// スクロールしたら
+  border();// 関数を実行
+});
+
+
+// 下から出てくる
+function fadeIn() {
+  $('.fadeUpTrigger').each(function () {// fadeUpTriggerクラスの各要素に対して
+    let scroll = $(window).scrollTop();// スクロール位置を取得する
+    let triTop = $(this).offset().top + 100;// 要素の上部より100px下の位置を取得
+    let winHeight = $(window).height();// ウィンドウの高さを取得
+    if (scroll >= triTop - winHeight) { // 画面内に要素が入ったかを判断
+      $(this).addClass('fadeUp');// fadeUpクラスを付与
+    } else {
+      $(this).removeClass('fadeUp');// fadeUpクラスを削除
+    }
+  });
+}
+$(window).scroll(function () {
+  fadeIn();
+});
